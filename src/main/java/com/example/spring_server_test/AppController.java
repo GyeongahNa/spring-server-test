29844b74fde9d46acc56772c46ee9517410d4f55
나경아 <gyeongah.na@navercorp.com>
2025-03-17 14:57:30 +0900

    first commit

---
+++ src/main/java/com/example/spring_server_test/AppController.java
@@ -0,0 +1,12 @@
+package com.example.spring_server_test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+	@GetMapping("/")
+	public String home() {
+		return "Test Hello World!";
+	}
+}
---
+++ src/main/java/com/example/spring_server_test/SpringServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_server_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServerTestApplication.class, args);
+	}
+
+}
